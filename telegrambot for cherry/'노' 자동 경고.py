401af6c4da13d5ad14b6d70dc4a6d790fe9e5a8c
lalala <lamon8072@gmail.com>
2023-08-14 02:35:04 +0900

    virsion 1( 도움말 주석 추가)

--- telegrambot for cherry/'노' 자동 경고.py
+++ telegrambot for cherry/'노' 자동 경고.py
@@ -4,6 +4,8 @@ import io
 
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+# 이 위는 인코딩형식을 지정하는 함수로 건드리지 말 것
+# 만약 인코딩이 깨졌다면  맨 윗줄을 -*- coding: eur-kr -*- 로 바꾸고 다시 실행해보기
 
 import logging
 from telegram import Update
@@ -19,14 +21,16 @@ def start(update, context):
 
 def message_handler(update: Update, context: CallbackContext):
     text = update.message.text.lower()
-    if text.endswith('노') and not text.endswith('야노'):
+    if text.endswith('노') and not text.endswith('야노'): # and not뒤 부터는 예외를 지정함
+       # 예외를 추가 할땐 and not text.endswith('야노')와 같이 작성하기
+       # 출력될 텍스트를 입력하세요,\n 뒤는 줄바꿈 되어서 출력됨
        update.message.reply_text('@JunetoAugust @Cherry902040 @RyjiOlllllllllllllllllllllllllll @Ahndangdang \n 너... 썼구나...?') 
         
 
 def test(update: Update, context: CallbackContext):
     user_name = update.message.from_user.name
     
-
+# 여기는 건드리지 말 것
 # 메인 함수
 def main():
     # 로깅 설정
